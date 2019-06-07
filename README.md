77300ee76bab257897f223efc77656d05a7ab1a7
unknown <cortikalch@hotmail.com>
2019-06-06 23:04:58 -0500

    Customer final version

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Bamazon
-A CLI who interact with mysql and node.js to make a program for a store
+This is a simple command line application of an Amazon-like store front using Node.js and MySQL. This exercise is for the customer interface, this app let the user choise a product from the table of the store, select the item by ID and bring to the program the quantity would purchase, the code compare if have enougth stock of that product, in afirmative case show the total cost, but if the stock is insufficient displayed the message to the user and display the table of products again.
+
+## Getting Started
+In order to run this application you should have the MySQL database installed and ready on your machine. If you have set up correctly on your computer MySQL, follow the next steps:
+- Clone the repo
+- Open the Terminal or Git Bash to install the npm packages neededs, in order to do this run the follow command:
+    - 'npm i mysql'
+    - 'npm i inquirer'
+    - 'npm i cli-table'
+    - 'npm i figlet'
+- Run the program with the command 'node bamazonCustomer.js'
+
+## Running the test
+It looks like this in action
+
+![](Bamazon.gif)
+
+## Technologies used
+- Node.js
+- Inquire NPM Package
+- MySQL NPM Package
+- CLI Table NPM Package
+- Figlet NPM Package
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,13 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require("cli-table");
+var figlet = require('figlet');
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password:"",
+    password:"chDCV305098698_",
     database: "bamazon_db"
 });
 
@@ -16,69 +18,74 @@ connection.connect(function(err){
 });
 
 
-function initialProducts() {
-    console.log("THE BAMAZON STORE \n");
+var initialProducts = function() {
+    console.log(figlet.textSync("BAMAZON", {
+        font: "Standard",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+    }));    
     connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        var displayProducts = new Table ({
+            head: ["ID", "Product Name", "Department", "Price", "Stock"],
+            colWidths: [5, 30, 25, 10, 10]
+        });
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+            displayProducts.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
         }
-        console.log("---------------------------------------");
-        choiceProduct();
+        console.log(displayProducts.toString());
+        choiceProduct(); 
     });
 }
 
-var choiceProduct = function(){
-    connection.query("SELECT * FROM products", function(err,res){
-        console.log("FIRST RES!!!" + res);
-        var resProd = res; 
+function choiceProduct(){
         inquirer.prompt([
             {
                 name: "IDitemInput",
                 type: "input",
-                message: "Write the ID of the product you would like to buy",
-                validate: function(value){
-                    if(isNaN(value)==false){
-                        return true;
-                    }else {
-                        return false;
-                    }
-                }
+                message: "Choice the product you would like to buy",
+                filter: Number
+            },
+            {
+                name: "unitsInput",
+                type: "input",
+                message: "How many units would like?",
+                filter: Number
+                
             }
         ]).then(function(answer){
-            connection.query("SELECT * FROM products", function(res,err){
-                console.log("THIS IS RESPRODDD!"+ resProd);
-                for (var i = 0; i < resProd.length; i++) {
-                    if(res[i].item_id == answer.IDitemInput) {
-                        inquirer.prompt([
-                            {
-                                name: "unitsInput",
-                                type: "input",
-                                message: "How many units would like?",
-                                validate: function(value){
-                                    if(isNaN(value)==false){
-                                        return true;
-                                    }else {
-                                        return false;
-                                    }
-                                }
-                                
-                            }
-                        ]).then(function(answer){
-                            connection.query("SELECT * FROM products", function(res, err){
-                                if(answer.stock_quantity < res[i].stock_quantity){
-                                    console.log("Succesfuly buy it!");
-                                } else {
-                                    console.log("Insufficient quantity! :(");
-                                }
-                            });
-                        });
-                    }    
-                    
-                }
-                
-            });
+            var quantityNeed = answer.unitsInput;
+            var IDreq = answer.IDitemInput;
+            orderProduct(IDreq, quantityNeed);
+        });
+};
 
-        });       
-    });
+
+function orderProduct(IDitemInput, amtNeed){
+    connection.query("SELECT * FROM products WHERE item_id = " + IDitemInput, function(err, res){
+        if (err) throw err;
+        if(amtNeed <= res[0].stock_quantity){
+            var totalCost = res[0].price * amtNeed;
+            console.log("Your product is on stock!");
+            console.log("Your total for " + amtNeed + " " + res[0].product_name + " is $ " + totalCost);
+            console.log("---------------------------------------------");
+            var newStock = res[0].stock_quantity - amtNeed;
+            connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: newStock
+                    },
+                    {
+                        item_id: IDitemInput
+                    }
+                ], 
+                ); 
+        } else {
+            console.log("Sorry, insufficent quantity of " + res[0].product_name);
+        };
+        initialProducts();
+    }); 
 }
-
